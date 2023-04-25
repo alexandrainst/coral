2624bf1771e0985eb68f11649c791d98b65075b1
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-04-25 17:32:22 +0200

    tests: Add unit test scaffolding for asr.wav2vec2.clean module

--- tests/test_asr__wav2vec2__clean.py
+++ tests/test_asr__wav2vec2__clean.py
@@ -1,1 +1,15 @@
 """Unit tests for the `asr.wav2vec2.clean` module."""
+
+from coral_models.asr.wav2vec2.clean import clean_dataset, clean_transcription
+
+
+class TestCleanDataset:
+    """Unit tests for the `clean_dataset` function."""
+
+    pass
+
+
+class TestCleanTranscription:
+    """Unit tests for the `clean_transcription` function."""
+
+    pass
