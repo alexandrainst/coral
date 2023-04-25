356906420ff3310e05333506d766b9638a17e0cd
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-04-25 16:26:56 +0200

    chore: Add logging to main __init__ file

--- src/coral_models/__init__.py
+++ src/coral_models/__init__.py
@@ -3,6 +3,20 @@
 """
 
 import importlib.metadata
+import logging
+
+from .asr import finetune_wav2vec2, finetune_whisper
+from .utils import block_terminal_output
 
 # Fetches the version of the package as defined in pyproject.toml
 __version__ = importlib.metadata.version(__package__)
+
+# Block terminal output
+block_terminal_output()
+
+# Set up logging
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s] <%(name)s> %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
