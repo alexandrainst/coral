34f719449fb179bd7c5b8f276c95c68bc9d8061a
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-04-26 17:34:11 +0200

    fix: Logging setup with hydra

--- src/coral_models/__init__.py
+++ src/coral_models/__init__.py
@@ -3,7 +3,6 @@
 """
 
 import importlib.metadata
-import logging
 import sys
 
 from .asr import finetune_wav2vec2
@@ -15,10 +14,3 @@ __version__ = importlib.metadata.version(__package__)
 # Block terminal output, if we are not running tests
 if not hasattr(sys, "_called_from_test"):
     block_terminal_output()
-
-# Set up logging
-logging.basicConfig(
-    format="%(asctime)s [%(levelname)s] <%(name)s> %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    level=logging.INFO,
-)
