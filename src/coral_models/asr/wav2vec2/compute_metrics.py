fc3c202a65170baa1740aae804028dce43060ec7
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-05-01 11:43:39 +0200

    style: Comments

--- src/coral_models/asr/wav2vec2/compute_metrics.py
+++ src/coral_models/asr/wav2vec2/compute_metrics.py
@@ -24,24 +24,20 @@ def compute_metrics(
             dictionary with 'wer' as the key and the word error rate as the
             value.
     """
-    # Intitialise the metric
     wer_metric = evaluate.load("wer")
+    pad_token = processor.tokenizer.pad_token_id
 
-    # Get the padding token
-    pad_token: int = processor.tokenizer.pad_token_id
-
-    # Set the ground truth labels with label id -100 to be the padding token id
-    pred.label_ids[pred.label_ids == -100] = pad_token
-
-    # Get the ids of the predictions
     pred_logits = pred.predictions
+    assert not isinstance(pred_logits, tuple)
 
-    # In cases where the logits are -100 for all characters, convert the logits
-    # for the padding character to be 100, to make sure that padding is chosen
-    pad_arr = pred.predictions[:, :, pad_token]
-    pred.predictions[:, :, pad_token] = np.where(pad_arr == -100, 100, pad_arr)
+    # In cases where the prediction logits are -100 for all characters, convert the
+    # logits for the padding character to be 100, to make sure that padding is chosen
+    pad_arr = pred_logits[:, :, pad_token]
+    pred_logits[:, :, pad_token] = np.where(pad_arr == -100, 100, pad_arr)
 
-    # Decode the predictions, to get the transcriptions
+    # Decode the predictions to get the transcriptions. When a language model is
+    # attached to the processor then we get the predicted string directly from the
+    # logits
     if isinstance(processor, Wav2Vec2ProcessorWithLM):
         try:
             pred_str = processor.batch_decode(pred_logits).text
@@ -54,18 +50,28 @@ def compute_metrics(
             mismatch_dim = len(vocab_size) - pred_logits.shape[-1]
             pred_logits = np.pad(pred_logits, ((0, 0), (0, 0), (0, mismatch_dim)))
             pred_str = processor.batch_decode(pred_logits).text
+
+    # Otherwise, if we are not using a language model, we need to convert the logits to
+    # token IDs and then decode the token IDs to get the predicted string
     else:
         pred_ids = np.argmax(pred_logits, axis=-1)
         pred_str = processor.batch_decode(pred_ids)
 
+    # Set the ground truth labels with label id -100 to be the padding token id. This
+    # ensures that the WER metric does not consider these labels in its computation.
+    labels = pred.label_ids
+    assert isinstance(labels, np.ndarray)
+    labels[labels == -100] = pad_token
+
     # Decode the ground truth labels
-    label_str = processor.tokenizer.batch_decode(pred.label_ids, group_tokens=False)
+    label_str = processor.tokenizer.batch_decode(labels, group_tokens=False)
 
     # Compute the word error rate
     computed = wer_metric.compute(predictions=pred_str, references=label_str)
     assert computed is not None
 
-    # Ensure that `wer` is a dict
+    # Ensure that `wer` is a dict, as metrics in the `evaluate` library can either be
+    # dicts or floats
     if not isinstance(computed, dict):
         return dict(wer=computed)
     else:
