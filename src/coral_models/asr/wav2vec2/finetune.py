c7ebade24ba315e0e7b10a73183f1f924a3dba44
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-05-01 16:17:32 +0200

    refactor: Split up `finetune_wav2vec2` into smaller functions

--- src/coral_models/asr/wav2vec2/finetune.py
+++ src/coral_models/asr/wav2vec2/finetune.py
@@ -3,11 +3,11 @@
 import logging
 from functools import partial
 
+from datasets import DatasetDict
 from omegaconf import DictConfig
 from torch.backends.mps import is_available as mps_is_available
 from transformers import (
     EarlyStoppingCallback,
-    PreTrainedTokenizerBase,
     Trainer,
     TrainerCallback,
     TrainingArguments,
@@ -21,7 +21,7 @@ from ..utils import dump_vocabulary
 from .clean import clean_dataset
 from .compute_metrics import compute_metrics
 from .data_collator import DataCollatorCTCWithPadding
-from .preprocess import load_processor
+from .preprocess import ModifiedWav2Vec2Processor, load_processor
 
 logger = logging.getLogger(__name__)
 
@@ -32,6 +32,49 @@ def finetune_wav2vec2(cfg: DictConfig) -> None:
     Args:
         cfg (DictConfig):
             The Hydra cfguration object.
+    """
+    dataset, processor = load_preprocessed_dataset(cfg)
+    model = load_model(cfg, processor=processor)
+
+    logger.debug("Initialising trainer...")
+    trainer = Trainer(
+        model=model,
+        data_collator=load_data_collator(processor=processor),
+        args=load_training_args(cfg),
+        compute_metrics=partial(compute_metrics, processor=processor),
+        train_dataset=dataset["train"],  # type: ignore
+        eval_dataset=dataset["val"],  # type: ignore
+        tokenizer=processor.tokenizer,
+        callbacks=load_callbacks(cfg),
+    )
+
+    logger.debug("Saving preprocessor...")
+    processor.save_pretrained(cfg.model_dir)
+
+    trainer.train(resume_from_checkpoint=cfg.resume_from_checkpoint)
+
+    logger.info("Saving model...")
+    model.save_pretrained(cfg.model_dir)
+
+    if cfg.push_to_hub:
+        logger.info("Pushing model to the hub...")
+        trainer.push_to_hub()
+
+    # TODO: Add ngram model
+
+
+def load_preprocessed_dataset(
+    cfg: DictConfig,
+) -> tuple[DatasetDict, ModifiedWav2Vec2Processor]:
+    """Load the dataset, clean it and preprocess it.
+
+    Args:
+        cfg (DictConfig):
+            The Hydra configuration object.
+
+    Returns:
+        pair of DatasetDict and ModifiedWav2Vec2Processor:
+            The preprocessed dataset, and the processor used to preprocess it.
     """
     logger.info("Loading dataset...")
     dataset = load_data(cfg)
@@ -48,38 +91,22 @@ def finetune_wav2vec2(cfg: DictConfig) -> None:
     processor = load_processor(cfg)
     dataset = processor.preprocess(dataset=dataset)
 
-    logger.debug("Initialising data collator...")
-    data_collator = DataCollatorCTCWithPadding(processor=processor, padding="longest")
+    return dataset, processor
 
-    logger.info("Initialising model...")
-    tokenizer: PreTrainedTokenizerBase = processor.tokenizer
-    with ignore_transformers_output():
-        model = Wav2Vec2ForCTC.from_pretrained(
-            cfg.model.pretrained_model_id,
-            activation_dropout=cfg.model.activation_dropout,
-            attention_dropout=cfg.model.attention_dropout,
-            hidden_dropout=cfg.model.hidden_dropout,
-            feat_proj_dropout=cfg.model.feat_proj_dropout,
-            final_dropout=cfg.model.final_dropout,
-            mask_time_prob=cfg.model.mask_time_prob,
-            mask_feature_prob=cfg.model.mask_feature_prob,
-            mask_feature_length=cfg.model.mask_feature_length,
-            layerdrop=cfg.model.layerdrop,
-            ctc_loss_reduction=cfg.model.ctc_loss_reduction,
-            pad_token_id=tokenizer.pad_token_id,
-            bos_token_id=tokenizer.bos_token_id,
-            eos_token_id=tokenizer.eos_token_id,
-            vocab_size=len(tokenizer.get_vocab()),
-        )
-        assert isinstance(model, Wav2Vec2ForCTC)
 
-    if cfg.model.freeze_feature_encoder:
-        logger.debug("Freezing feature encoder...")
-        for param in model.wav2vec2.parameters():
-            param.requires_grad = False
+def load_training_args(cfg: DictConfig) -> TrainingArguments:
+    """Load the training arguments for the Trainer.
 
+    Args:
+        cfg (DictConfig):
+            The Hydra configuration object.
+
+    Returns:
+        TrainingArguments:
+            The training arguments.
+    """
     logger.debug("Initialising training arguments...")
-    training_args = TrainingArguments(
+    return TrainingArguments(
         output_dir=cfg.model_dir,
         hub_model_id=cfg.hub_id,
         per_device_train_batch_size=cfg.model.batch_size,
@@ -108,6 +135,60 @@ def finetune_wav2vec2(cfg: DictConfig) -> None:
         report_to=[],
     )
 
+
+def load_model(cfg: DictConfig, processor: ModifiedWav2Vec2Processor) -> Wav2Vec2ForCTC:
+    """Load the Wav2Vec 2.0 model.
+
+    Args:
+        cfg (DictConfig):
+            The Hydra configuration object.
+        processor (ModifiedWav2Vec2Processor):
+            The processor object.
+
+    Returns:
+        Wav2Vec2ForCTC:
+            The Wav2Vec 2.0 model.
+    """
+    with ignore_transformers_output():
+        logger.info("Initialising model...")
+        model = Wav2Vec2ForCTC.from_pretrained(
+            cfg.model.pretrained_model_id,
+            activation_dropout=cfg.model.activation_dropout,
+            attention_dropout=cfg.model.attention_dropout,
+            hidden_dropout=cfg.model.hidden_dropout,
+            feat_proj_dropout=cfg.model.feat_proj_dropout,
+            final_dropout=cfg.model.final_dropout,
+            mask_time_prob=cfg.model.mask_time_prob,
+            mask_feature_prob=cfg.model.mask_feature_prob,
+            mask_feature_length=cfg.model.mask_feature_length,
+            layerdrop=cfg.model.layerdrop,
+            ctc_loss_reduction=cfg.model.ctc_loss_reduction,
+            pad_token_id=processor.tokenizer.pad_token_id,
+            bos_token_id=processor.tokenizer.bos_token_id,
+            eos_token_id=processor.tokenizer.eos_token_id,
+            vocab_size=len(processor.tokenizer.get_vocab()),
+        )
+        assert isinstance(model, Wav2Vec2ForCTC)
+
+    if cfg.model.freeze_feature_encoder:
+        logger.debug("Freezing feature encoder...")
+        for param in model.wav2vec2.parameters():
+            param.requires_grad = False
+
+    return model
+
+
+def load_callbacks(cfg: DictConfig) -> list[TrainerCallback]:
+    """Load the callbacks for the Trainer.
+
+    Args:
+        cfg (DictConfig):
+            The Hydra configuration object.
+
+    Returns:
+        list of TrainerCallback:
+            The callbacks.
+    """
     callbacks: list[TrainerCallback] = list()
     if cfg.model.early_stopping:
         logger.debug("Initialising early stopping callback...")
@@ -115,29 +196,21 @@ def finetune_wav2vec2(cfg: DictConfig) -> None:
             early_stopping_patience=cfg.model.early_stopping_patience
         )
         callbacks = [early_stopping_callback]
+    return callbacks
 
-    logger.debug("Initialising trainer...")
-    trainer = Trainer(
-        model=model,
-        data_collator=data_collator,
-        args=training_args,
-        compute_metrics=partial(compute_metrics, processor=processor),
-        train_dataset=dataset["train"],  # type: ignore
-        eval_dataset=dataset["val"],  # type: ignore
-        tokenizer=tokenizer,
-        callbacks=callbacks,
-    )
 
-    logger.debug("Saving preprocessor...")
-    processor.save_pretrained(cfg.model_dir)
+def load_data_collator(
+    processor: ModifiedWav2Vec2Processor,
+) -> DataCollatorCTCWithPadding:
+    """Load the data collator.
 
-    trainer.train(resume_from_checkpoint=cfg.resume_from_checkpoint)
+    Args:
+        processor (ModifiedWav2Vec2Processor):
+            The processor object.
 
-    logger.info("Saving model...")
-    model.save_pretrained(cfg.model_dir)
-
-    if cfg.push_to_hub:
-        logger.info("Pushing model to the hub...")
-        trainer.push_to_hub()
-
-    # TODO: Add ngram model
+    Returns:
+        DataCollatorCTCWithPadding:
+            The data collator.
+    """
+    logger.debug("Initialising data collator...")
+    return DataCollatorCTCWithPadding(processor=processor, padding="longest")
