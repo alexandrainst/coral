23452e1b41dab0694d5e2db33b781b965168f5ae
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-05-04 21:38:56 +0200

    feat: Add `build_ftspeech` script

--- src/coral_models/asr/ftspeech.py
+++ src/coral_models/asr/ftspeech.py
@@ -6,9 +6,90 @@ from pathlib import Path
 import pandas as pd
 from datasets import Audio, Dataset, DatasetDict
 from joblib import Parallel, delayed
-from omegaconf import DictConfig
 from pydub import AudioSegment
 from tqdm.auto import tqdm
+
+
+def build_and_store_data(input_dir: Path | str, output_dir: Path | str) -> None:
+    """Builds and saves the FTSpeech dataset.
+
+    Args:
+        input_dir (str or Path):
+            The directory where the raw dataset is stored.
+        output_dir (str or Path):
+            The path to the resulting dataset.
+
+    Raises:
+        FileNotFoundError:
+            If `input_dir` does not exist.
+    """
+    # Ensure that the paths are Path objects
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+
+    if not input_dir.exists():
+        raise FileNotFoundError(f"{input_dir} does not exist")
+
+    # These are the paths to the transcription files
+    paths = {
+        "train": input_dir / "text" / "ft-speech_train.tsv",
+        "dev_balanced": input_dir / "text" / "ft-speech_dev-balanced.tsv",
+        "dev_other": input_dir / "text" / "ft-speech_dev-other.tsv",
+        "test_balanced": input_dir / "text" / "ft-speech_test-balanced.tsv",
+        "test_other": input_dir / "text" / "ft-speech_test-other.tsv",
+    }
+
+    # Load files with transcriptions
+    dfs = {split: pd.read_csv(path, sep="\t") for split, path in paths.items()}
+
+    # Preprocess the transcriptions
+    for split, df in dfs.items():
+        df["sentence"] = df.transcript.map(preprocess_transcription)
+        dfs[split] = df
+
+    # Add a `speaker_id` column to the dataframes
+    for split, df in dfs.items():
+        df["speaker_id"] = [row.utterance_id.split("_")[0] for _, row in df.iterrows()]
+        dfs[split] = df
+
+    # Ensure that `processed_audio` exists
+    processed_audio_path = input_dir / "processed_audio"
+    processed_audio_path.mkdir(exist_ok=True)
+
+    # Split the audio files
+    for split, df in tqdm(list(dfs.items()), desc="Splitting audio"):
+        with tqdm(df.to_dict("records"), desc=split, leave=False) as pbar:
+            with Parallel(n_jobs=mp.cpu_count()) as parallel:
+                parallel(delayed(split_audio)(record, input_dir) for record in pbar)
+
+    # Add an `audio` column to the dataframes, containing the paths to the audio files
+    for split, df in dfs.items():
+        audio_paths: list[str] = list()
+        for _, row in df.iterrows():
+            filename: str = row.utterance_id + ".wav"
+            audio_path: Path = input_dir / "processed_audio" / filename
+            audio_paths.append(str(audio_path.resolve()))
+        df["audio"] = audio_paths
+        dfs[split] = df
+
+    # Remove unused columns
+    cols_to_drop = ["utterance_id", "start_time", "end_time", "transcript"]
+    for split, df in dfs.items():
+        df = df.drop(columns=cols_to_drop)
+        dfs[split] = df
+
+    # Convert the dataframe to a HuggingFace Dataset
+    datasets = {
+        split: Dataset.from_pandas(df, preserve_index=False)
+        for split, df in dfs.items()
+    }
+    dataset = DatasetDict(datasets)
+
+    # Cast `audio` as the audio path column, which ensures that the audio can be loaded
+    # seamlessly on the fly
+    dataset = dataset.cast_column("audio", Audio(sampling_rate=16_000))
+
+    dataset.save_to_disk(str(output_dir))
 
 
 def preprocess_transcription(transcription: str) -> str:
@@ -64,91 +145,3 @@ def split_audio(record: dict, input_path: str | Path) -> None:
     out_ = audio.export(str(new_audio_path.resolve()), format="wav")
     out_.close()
     del audio
-
-
-def build_and_store_data(
-    cfg: DictConfig, input_dir: Path | str, output_dir: Path | str
-) -> None:
-    """Builds and saves the FTSpeech dataset.
-
-    Args:
-        cfg (DictConfig):
-            The Hydra configuration object.
-        input_dir (str or Path):
-            The directory where the raw dataset is stored.
-        output_dir (str or Path):
-            The path to the resulting dataset.
-
-    Raises:
-        FileNotFoundError:
-            If `input_dir` does not exist.
-    """
-    # Ensure that the paths are Path objects
-    input_dir = Path(input_dir)
-    output_dir = Path(output_dir)
-
-    if not input_dir.exists():
-        raise FileNotFoundError(f"{input_dir} does not exist")
-
-    # These are the paths to the transcription files
-    paths = {
-        "train": input_dir / "text" / "ft-speech_train.tsv",
-        "dev_balanced": input_dir / "text" / "ft-speech_dev-balanced.tsv",
-        "dev_other": input_dir / "text" / "ft-speech_dev-other.tsv",
-        "test_balanced": input_dir / "text" / "ft-speech_test-balanced.tsv",
-        "test_other": input_dir / "text" / "ft-speech_test-other.tsv",
-    }
-
-    # Load files with transcriptions
-    dfs = {split: pd.read_csv(path, sep="\t") for split, path in paths.items()}
-
-    # Preprocess the transcriptions
-    for split, df in dfs.items():
-        df[cfg.dataset.text_column] = df.transcript.map(preprocess_transcription)
-        dfs[split] = df
-
-    # Add a `speaker_id` column to the dataframes
-    for split, df in dfs.items():
-        df["speaker_id"] = [row.utterance_id.split("_")[0] for _, row in df.iterrows()]
-        dfs[split] = df
-
-    # Ensure that `processed_audio` exists
-    processed_audio_path = input_dir / "processed_audio"
-    if not processed_audio_path.exists():
-        processed_audio_path.mkdir()
-
-    # Split the audio files
-    for split, df in tqdm(list(dfs.items()), desc="Splitting audio"):
-        pbar = tqdm(df.to_dict("records"), desc=split, leave=False)
-        split_fn = delayed(split_audio)
-        with Parallel(n_jobs=mp.cpu_count()) as parallel:
-            parallel(split_fn(record, input_dir) for record in pbar)
-
-    # Add an `audio` column to the dataframes, containing the paths to the audio files
-    for split, df in dfs.items():
-        audio_paths = list()
-        for _, row in df.iterrows():
-            filename = row.utterance_id + ".wav"
-            audio_path = input_dir / "processed_audio" / filename
-            audio_paths.append(str(audio_path.resolve()))
-        df["audio"] = audio_paths
-        dfs[split] = df
-
-    # Remove unused columns
-    cols_to_drop = ["utterance_id", "start_time", "end_time", "transcript"]
-    for split, df in dfs.items():
-        df = df.drop(columns=cols_to_drop)
-        dfs[split] = df
-
-    # Convert the dataframe to a HuggingFace Dataset
-    datasets = {
-        split: Dataset.from_pandas(df, preserve_index=False)
-        for split, df in dfs.items()
-    }
-    dataset = DatasetDict(datasets)
-
-    # Cast `audio` as the audio path column, which ensures that the audio can be loaded
-    # seamlessly on the fly
-    dataset = dataset.cast_column("audio", Audio(sampling_rate=16_000))
-
-    dataset.save_to_disk(str(output_dir))
---
+++ src/scripts/build_ftspeech.py
@@ -0,0 +1,28 @@
+"""Script that builds the FTSpeech dataset from the raw data.
+
+Usage:
+    build_ftspeech.py <raw_data_dir> <output_dir>
+"""
+
+import click
+
+from coral_models.asr.ftspeech import build_and_store_data
+
+
+@click.command()
+@click.argument("raw_data_dir", type=click.Path(exists=True))
+@click.argument("output_dir", type=click.Path())
+def main(raw_data_dir: str, output_dir: str) -> None:
+    """Builds and stores the FTSpeech dataset.
+
+    Args:
+        raw_data_dir (str):
+            The directory where the raw dataset is stored.
+        output_dir (str):
+            The path to the resulting dataset.
+    """
+    build_and_store_data(input_dir=raw_data_dir, output_dir=output_dir)
+
+
+if __name__ == "__main__":
+    main()
