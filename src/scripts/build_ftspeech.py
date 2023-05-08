89c25ba79410bd85a0144b9a86abd811412c5354
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-05-08 20:18:47 +0200

    fix: Remove n_jobs from script

--- src/scripts/build_ftspeech.py
+++ src/scripts/build_ftspeech.py
@@ -12,8 +12,7 @@ from coral_models.asr.ftspeech import build_and_store_data
 @click.command()
 @click.argument("raw_data_dir", type=click.Path(exists=True))
 @click.argument("output_dir", type=click.Path())
-@click.option("--n-jobs", "-n", type=int, default=-1)
-def main(raw_data_dir: str, output_dir: str, n_jobs: int) -> None:
+def main(raw_data_dir: str, output_dir: str) -> None:
     """Builds and stores the FTSpeech dataset.
 
     Args:
@@ -21,12 +20,8 @@ def main(raw_data_dir: str, output_dir: str, n_jobs: int) -> None:
             The directory where the raw dataset is stored.
         output_dir (str):
             The path to the resulting dataset.
-        n_jobs (int, optional):
-            The number of jobs to use for parallel processing. Can be a negative number
-            to use all available cores minus `n_jobs`. Defaults to -1, meaning all
-            available cores minus 1.
     """
-    build_and_store_data(input_dir=raw_data_dir, output_dir=output_dir, n_jobs=n_jobs)
+    build_and_store_data(input_dir=raw_data_dir, output_dir=output_dir)
 
 
 if __name__ == "__main__":
