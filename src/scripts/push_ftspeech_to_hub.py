756452a7a9f98e1fd4d46e19c2b9281ecdb6f8eb
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-05-11 12:37:04 +0200

    feat: Add `--private` flag to `push_ftspeech_to_hub`, and add help strings

--- src/scripts/push_ftspeech_to_hub.py
+++ src/scripts/push_ftspeech_to_hub.py
@@ -9,9 +9,26 @@ from datasets import DatasetDict
 
 
 @click.command()
-@click.argument("compiled_dataset_dir", type=click.Path(exists=True))
-@click.option("--dataset-id", type=str, default="alexandrainst/ftspeech")
-def main(compiled_data_dir: str, dataset_id: str) -> None:
+@click.argument(
+    "compiled-dataset-dir",
+    type=click.Path(exists=True),
+    help="The directory where the compiled dataset is stored.",
+)
+@click.option(
+    "--dataset-id",
+    type=str,
+    default="alexandrainst/ftspeech",
+    show_default=True,
+    help="The ID of the dataset on the Hugging Face Hub.",
+)
+@click.option(
+    "--private",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Whether to make the dataset private on the Hugging Face Hub.",
+)
+def main(compiled_data_dir: str, dataset_id: str, private: bool) -> None:
     """Builds and stores the FTSpeech dataset.
 
     This also catches RuntimeError exceptions which tends to happen during the upload
@@ -22,11 +39,17 @@ def main(compiled_data_dir: str, dataset_id: str) -> None:
             The directory where the compiled dataset is stored.
         dataset_id (str):
             The ID of the dataset on the Hugging Face Hub.
+        private (bool):
+            Whether to make the dataset private.
     """
     dataset = DatasetDict.load_from_disk(compiled_data_dir)
     while True:
         try:
-            dataset.push_to_hub(repo_id=dataset_id, max_shard_size="50MB")
+            dataset.push_to_hub(
+                repo_id=dataset_id,
+                max_shard_size="50MB",
+                private=private,
+            )
             break
         except RuntimeError:
             pass
