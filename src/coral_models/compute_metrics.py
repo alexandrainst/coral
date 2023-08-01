7c8269d9edad200a1d05a453ac23bc6ec9ba5f97
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-08-01 15:10:38 +0200

    fix: Allow 2-dimensional inputs to compute_metrics, to cater to Whisper models

--- src/coral_models/compute_metrics.py
+++ src/coral_models/compute_metrics.py
@@ -25,32 +25,43 @@ def compute_wer_metrics(pred: EvalPrediction, processor: Processor) -> dict[str,
     tokenizer: PreTrainedTokenizerBase = getattr(processor, "tokenizer")
     pad_token = tokenizer.pad_token_id
 
-    # Shape: [batch_size, seq_len, vocab_size]
+    # Shape: [batch_size, seq_len, vocab_size] or [batch_size, seq_len]
     predictions: NDArray[np.int_] | NDArray[np.float_] = pred.predictions
 
-    # In cases where the prediction logits are -100 for all characters, convert the
-    # logits for the padding character to be 100, to make sure that padding is chosen
-    # TODO: Does this actually do what we want it to do?
-    pad_arr = predictions[:, :, pad_token]
-    predictions[:, :, pad_token] = np.where(pad_arr == -100, 100, pad_arr)
+    if len(predictions.shape) == 3:
+        # In cases where the prediction logits are -100 for all characters, convert the
+        # logits for the padding character to be 100, to make sure that padding is
+        # chosen
+        # TODO: Does this actually do what we want it to do?
+        pad_arr = predictions[:, :, pad_token]
+        predictions[:, :, pad_token] = np.where(pad_arr == -100, 100, pad_arr)
 
-    # Decode the predictions to get the transcriptions. When a language model is
-    # attached to the processor then we get the predicted string directly from the
-    # logits
-    if predictions.dtype == np.int_:
-        # If the vocabulary dimension of the predictions is too small then we pad with
-        # zeros to match the size of the vocabulary
-        vocab_size = tokenizer.get_vocab()
-        mismatch_dim = len(vocab_size) - predictions.shape[-1]
-        predictions = np.pad(predictions, ((0, 0), (0, 0), (0, mismatch_dim)))
+        # Decode the predictions to get the transcriptions. When a language model is
+        # attached to the processor then we get the predicted string directly from the
+        # logits. If the vocabulary dimension of the predictions is too small then we
+        # pad with zeros to match the size of the vocabulary
+        if predictions.dtype == np.int_:
+            vocab_size = tokenizer.get_vocab()
+            mismatch_dim = len(vocab_size) - predictions.shape[-1]
+            predictions = np.pad(predictions, ((0, 0), (0, 0), (0, mismatch_dim)))
+            predictions_str = tokenizer.batch_decode(
+                predictions, skip_special_tokens=True
+            )
 
-        predictions_str = [processor.decode(token_ids) for token_ids in predictions]
+        # Otherwise, if we are not using a language model, we need to convert the
+        # logits to token IDs and then decode the token IDs to get the predicted string
+        else:
+            pred_ids: NDArray[np.int_] = np.argmax(predictions, axis=-1)
+            predictions_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
 
-    # Otherwise, if we are not using a language model, we need to convert the logits to
-    # token IDs and then decode the token IDs to get the predicted string
+    elif len(predictions.shape) == 2 and predictions.dtype == np.int_:
+        predictions_str = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+
     else:
-        pred_ids: NDArray[np.int_] = np.argmax(predictions, axis=-1)
-        predictions_str = [processor.decode(ids) for ids in pred_ids]
+        raise ValueError(
+            f"Predictions have an unexpected shape {predictions.shape} and dtype "
+            f"{predictions.dtype}."
+        )
 
     # Set the ground truth labels with label id -100 to be the padding token id. This
     # ensures that the WER metric does not consider these labels in its computation.
@@ -59,7 +70,7 @@ def compute_wer_metrics(pred: EvalPrediction, processor: Processor) -> dict[str,
     labels[labels == -100] = pad_token
 
     # Decode the ground truth labels
-    labels_str = tokenizer.batch_decode(labels, group_tokens=False)
+    labels_str = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
     # Compute the word error rate
     computed = wer_metric.compute(predictions=predictions_str, references=labels_str)
