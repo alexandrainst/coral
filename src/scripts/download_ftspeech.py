7608c5d85e1c0367ff9b889554850a5cb440f753
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-08-07 13:56:58 +0200

    feat: Add `download_ftspeech` script

---
+++ src/scripts/download_ftspeech.py
@@ -0,0 +1,80 @@
+"""Script that downloads the FTSpeech corpus.
+
+Usage:
+    python download_ftspeech.py <download_dir>
+"""
+
+try:
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.firefox.options import Options
+except ImportError:
+    raise ImportError(
+        "To run this script, `selenium` needs to be installed. Please install it with "
+        "`pip install selenium` or `poetry add selenium`."
+    )
+
+import time
+from getpass import getpass
+from pathlib import Path
+
+import click
+
+
+@click.command("Downloads the FTSpeech corpus.")
+@click.argument("download_dir", type=click.Path())
+def main(download_dir) -> None:
+    """Downloads the FTSpeech corpus."""
+    download_dir = Path(download_dir)
+
+    options = Options()
+    options.add_argument("--headless")
+    options.set_preference("browser.download.folderList", 2)
+    options.set_preference("browser.download.manager.showWhenStarting", False)
+    options.set_preference("browser.download.dir", str(download_dir))
+    options.set_preference(
+        "browser.helperApps.neverAsk.saveToDisk", "application/x-gzip"
+    )
+    driver = webdriver.Firefox(options=options)
+
+    # Go to the FTSpeech download page
+    driver.get("http://130.226.140.126")
+
+    # Get the access token input field
+    access_token_input = driver.find_element(By.XPATH, '//input[@name="token"]')
+
+    # Get the access token
+    access_token = getpass("Access token: ")
+
+    # Enter the access token
+    access_token_input.send_keys(access_token)
+
+    # Get the download button
+    download_button = driver.find_element(By.XPATH, '//input[@type="submit"]')
+
+    # Click the download button
+    download_button.click()
+
+    # Wait for the download to finish
+    while len(list(download_dir.glob("*.part"))) > 0:
+        # Get the size of the part file
+        part_file_bytes = list(download_dir.glob("*.part"))[0].stat().st_size
+
+        # Convert the bytes to gigabytes
+        part_file_gbs = part_file_bytes / 1024 / 1024 / 1024
+
+        # Log the download progress
+        print(f"Downloading FTSpeech - {part_file_gbs:.2f} GB...", end="\r")
+
+        # Wait for a second
+        time.sleep(1)
+
+    # Wait for an hour, to ensure that it has finished downloading
+    time.sleep(60 * 60)
+
+    # Close the webdriver
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
