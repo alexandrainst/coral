5edb9dfac9bce79b815a4fa68265c70e0a1453d2
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-08-30 10:16:12 +0200

    feat: Top-level progress bar in `find_faulty_audio_clips`, replace print with log

--- src/scripts/find_faulty_audio_clips.py
+++ src/scripts/find_faulty_audio_clips.py
@@ -4,12 +4,16 @@ Usage:
     python src/scripts/find_faulty_audio_clips.py <path/to/audio/dir>
 """
 
+import logging
 import warnings
 from pathlib import Path
 
 import click
 import librosa
 from tqdm.auto import tqdm
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(message)s")
+logger = logging.getLogger(__name__)
 
 
 @click.command()
@@ -23,15 +27,19 @@ def main(audio_dir: str | Path) -> None:
     audio_dir = Path(audio_dir)
     folders = [folder for folder in audio_dir.iterdir() if folder.is_dir()]
     folders += [audio_dir]
-    for folder in folders:
-        for audio_file in tqdm(list(folder.glob("*.wav")), desc=f"Checking {folder}"):
+    for folder in tqdm(folders, desc=f"Checking {audio_dir} for faulty audio files"):
+        for audio_file in tqdm(
+            iterable=list(folder.glob("*.wav")),
+            desc=f"Checking {folder}",
+            leave=False,
+        ):
             try:
                 with warnings.catch_warnings():
                     warnings.simplefilter("ignore", category=UserWarning)
                     warnings.simplefilter("ignore", category=FutureWarning)
                     librosa.load(audio_file)
             except Exception as e:
-                print(audio_file, e)
+                logger.error(f"Could not open {audio_file!r}. The error was: {e}")
 
 
 if __name__ == "__main__":
