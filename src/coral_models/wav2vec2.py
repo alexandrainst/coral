c79d9f487f8ab2a62fe5786aecebe68468fbf705
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-10-26 14:26:31 +0200

    fix: Remove `ddp_backend`

--- src/coral_models/wav2vec2.py
+++ src/coral_models/wav2vec2.py
@@ -218,7 +218,7 @@ class Wav2Vec2ModelSetup:
             use_cpu=hasattr(sys, "_called_from_test"),
             dataloader_num_workers=self.cfg.dataloader_num_workers,
             ddp_find_unused_parameters=True,
-            ddp_backend="nccl",
+            # ddp_backend="nccl",
         )
         return args
 
