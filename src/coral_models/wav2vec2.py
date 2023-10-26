c351a13a80e59673b09e182160e3da59f4dc3548
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-10-26 15:58:46 +0200

    fix: Set ddp_find_unused_parameters to False

--- src/coral_models/wav2vec2.py
+++ src/coral_models/wav2vec2.py
@@ -225,7 +225,7 @@ class Wav2Vec2ModelSetup:
             save_safetensors=True,
             use_cpu=hasattr(sys, "_called_from_test"),
             dataloader_num_workers=self.cfg.dataloader_num_workers,
-            ddp_find_unused_parameters=True,
+            ddp_find_unused_parameters=False,
         )
         return args
 
