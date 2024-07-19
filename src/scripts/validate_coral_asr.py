ad4536c38cd9683ee82c70f1c28b49df084e2f54
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-07-19 13:03:31 +0200

    fix: Set not prefetch on validation

--- src/scripts/validate_coral_asr.py
+++ src/scripts/validate_coral_asr.py
@@ -173,6 +173,7 @@ def main(
             report_to=[],
             per_device_train_batch_size=1,
             per_device_eval_batch_size=1,
+            dataloader_prefetch_factor=0,
         ),
         model=model,
         data_collator=data_collator,
