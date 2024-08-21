a2237c9c775bb8476a0fd20efcdd45e6614a2868
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-08-21 17:27:28 +0200

    debug: Up truncation to 1000

--- src/scripts/validate_coral_asr.py
+++ src/scripts/validate_coral_asr.py
@@ -55,7 +55,7 @@ def main(config: DictConfig) -> None:
     assert isinstance(dataset, DatasetDict)
 
     # TEMP
-    dataset["train"] = dataset["train"].select(range(100))
+    dataset["train"] = dataset["train"].select(range(1000))
 
     logger.info("Resampling audio to 16kHz...")
     processed_dataset = dataset.cast_column(
