ae11f62e691d5058510869e42a0898f6d8cedb85
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-09-06 13:38:33 +0200

    refactor: Splitting up `evaluate` function into smaller pieces

--- src/coral/evaluate.py
+++ src/coral/evaluate.py
@@ -5,6 +5,7 @@ import logging
 
 import pandas as pd
 import torch
+from datasets import Dataset
 from dotenv import load_dotenv
 from omegaconf import DictConfig
 from transformers import AutomaticSpeechRecognitionPipeline, pipeline
@@ -32,12 +33,56 @@ def evaluate(config: DictConfig) -> pd.DataFrame:
         config.model_id is not None
     ), "`model_id` must be set to perform an evaluation!"
 
+    logger.info("Loading the dataset...")
     dataset = load_dataset_for_evaluation(config=config)
 
+    logger.info(f"Loading the {config.model_id!r} ASR model...")
+    transcriber = load_asr_pipeline(model_id=config.model_id)
+
+    logger.info("Computing the scores...")
+    _, _, all_scores = compute_metrics_of_dataset_using_pipeline(
+        dataset=dataset,
+        transcriber=transcriber,
+        metric_names=[config.metric],
+        characters_to_keep=config.characters_to_keep,
+        text_column="text",
+        audio_column="audio",
+        batch_size=config.batch_size,
+    )
+
+    logger.info(
+        "Converting the dataset to a dataframe computing the scores for each "
+        "metadata category..."
+    )
+    df = convert_evaluation_dataset_to_df(
+        dataset=dataset, sub_dialect_to_dialect_mapping=config.sub_dialect_to_dialect
+    )
+    df["score"] = all_scores[config.metric]
+    score_df = get_score_df(
+        df=df,
+        categories=["age_group", "gender", "dialect", "accent"],
+        metric_name=config.metric,
+    )
+    return score_df
+
+
+def convert_evaluation_dataset_to_df(
+    dataset: Dataset, sub_dialect_to_dialect_mapping: dict[str, str]
+) -> pd.DataFrame:
+    """Convert the evaluation dataset to a DataFrame.
+
+    Args:
+        dataset:
+            The evaluation dataset.
+        sub_dialect_to_dialect_mapping:
+            The mapping from sub-dialect to dialect.
+
+    Returns:
+        A DataFrame with the evaluation dataset.
+    """
     df = dataset.to_pandas()
     assert isinstance(df, pd.DataFrame)
 
-    # Make a new binary feature of whether the native language is Danish
     df["accent"] = df.country_birth.map(lambda x: "native" if x == "DK" else "foreign")
 
     age_group_mapping = {"0-25": (0, 25), "25-50": (26, 50), "50+": (50, None)}
@@ -49,15 +94,20 @@ def evaluate(config: DictConfig) -> pd.DataFrame:
         )
     )
 
-    df.dialect = df.dialect.map(config.sub_dialect_to_dialect)
+    df.dialect = df.dialect.map(sub_dialect_to_dialect_mapping)
+    return df
 
-    # Get unique values for each category
-    categories = ["age_group", "gender", "dialect", "accent"]
-    unique_category_values = [
-        df[f"{category}"].unique().tolist() + [None] for category in categories
-    ]
 
-    logger.info(f"Loading the {config.model_id!r} ASR model...")
+def load_asr_pipeline(model_id: str) -> AutomaticSpeechRecognitionPipeline:
+    """Load the ASR pipeline.
+
+    Args:
+        model_id:
+            The model ID to load.
+
+    Returns:
+        The ASR pipeline.
+    """
     if torch.cuda.is_available():
         device = torch.device("cuda")
     elif torch.backends.mps.is_available():
@@ -65,22 +115,33 @@ def evaluate(config: DictConfig) -> pd.DataFrame:
     else:
         device = torch.device("cpu")
     transcriber = pipeline(
-        task="automatic-speech-recognition", model=config.model_id, device=device
+        task="automatic-speech-recognition", model=model_id, device=device
     )
     assert isinstance(transcriber, AutomaticSpeechRecognitionPipeline)
+    return transcriber
 
-    _, _, all_scores = compute_metrics_of_dataset_using_pipeline(
-        dataset=dataset,
-        transcriber=transcriber,
-        metric_names=[config.metric],
-        characters_to_keep=config.characters_to_keep,
-        text_column="text",
-        audio_column="audio",
-        batch_size=config.batch_size,
-    )
-    df["score"] = all_scores[config.metric]
 
-    # Iterate over all combinations of categories
+def get_score_df(
+    df: pd.DataFrame, categories: list[str], metric_name: str
+) -> pd.DataFrame:
+    """Get the score DataFrame for the evaluation dataset.
+
+    Args:
+        df:
+            The evaluation dataframe, containing all the metadata columns and a 'score'
+            column.
+        categories:
+            The categories to evaluate.
+        metric_name:
+            The name of the metric to evaluate.
+
+    Returns:
+        The score DataFrame.
+    """
+    unique_category_values = [
+        df[f"{category}"].unique().tolist() + [None] for category in categories
+    ]
+
     records = list()
     for combination in it.product(*unique_category_values):
         # Apply the combination of filters
@@ -109,7 +170,7 @@ def evaluate(config: DictConfig) -> pd.DataFrame:
         )
         if combination_str == "":
             combination_str = "entire dataset"
-        score_str = f"{config.metric}={df_filtered.score.mean():.2f}"
+        score_str = f"{metric_name}={df_filtered.score.mean():.2f}"
         logger.info(f"Scores for {combination_str}: {score_str}")
 
     score_df = pd.DataFrame.from_records(data=records)
