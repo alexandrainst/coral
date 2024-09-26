5100bf50f7f7da88f8c311ef1a6356730007242c
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-09-26 15:41:42 +0200

    debug: Check if layerdrop and padding actually need changing in a multi-GPU setup

--- src/scripts/finetune_asr_model.py
+++ src/scripts/finetune_asr_model.py
@@ -37,13 +37,6 @@ def main(config: DictConfig) -> None:
     # hyperparameters
     is_main_process = os.getenv("RANK", "0") == "0"
     if os.getenv("WORLD_SIZE") is not None or torch.cuda.device_count() > 1:
-        if "layerdrop" in config.model and config.model.layerdrop != 0.0:
-            if is_main_process:
-                logger.info(
-                    "Forcing `layerdrop` to be 0.0 as this is required in a multi-GPU "
-                    "training"
-                )
-            config.model.layerdrop = 0.0
         if "gradient_checkpointing" in config and config.gradient_checkpointing is True:
             if is_main_process:
                 logger.info(
@@ -51,13 +44,20 @@ def main(config: DictConfig) -> None:
                     "GPU training"
                 )
             config.gradient_checkpointing = False
-        if config.padding != "max_length":
-            if is_main_process:
-                logger.info(
-                    "Forcing `padding` to be 'max_length' as this is required in a "
-                    "multi-GPU training"
-                )
-            config.padding = "max_length"
+        # if "layerdrop" in config.model and config.model.layerdrop != 0.0:
+        #     if is_main_process:
+        #         logger.info(
+        #             "Forcing `layerdrop` to be 0.0 as this is required in a multi-GPU "
+        #             "training"
+        #         )
+        #     config.model.layerdrop = 0.0
+        # if config.padding != "max_length":
+        #     if is_main_process:
+        #         logger.info(
+        #             "Forcing `padding` to be 'max_length' as this is required in a "
+        #             "multi-GPU training"
+        #         )
+        #     config.padding = "max_length"
 
     finetune(config=config)
 
