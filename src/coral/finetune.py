8d1c39ebc8f0e51916c21f5e0fc51b697a54d1e4
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-09-27 19:53:46 +0200

    debug

--- src/coral/finetune.py
+++ src/coral/finetune.py
@@ -15,6 +15,7 @@ from wandb.sdk.wandb_init import init as wandb_init
 from .data import load_data_for_finetuning
 from .data_models import ModelSetup
 from .model_setup import load_model_setup
+from .ngram import train_and_store_ngram_model
 from .utils import push_model_to_hub
 
 logger = logging.getLogger(__package__)
@@ -39,7 +40,6 @@ def finetune(config: DictConfig) -> None:
     # TEMP
     model = Wav2Vec2ForCTC.from_pretrained(config.model_dir)
     processor = Wav2Vec2ProcessorWithLM.from_pretrained(config.model_dir)
-    breakpoint()
 
     if config.wandb and is_main_process:
         wandb_init(
@@ -63,6 +63,7 @@ def finetune(config: DictConfig) -> None:
         callbacks=load_early_stopping_callback(config) if "val" in dataset else None,
     )
 
+    # TEMP
     # block_terminal_output()
     # with disable_tqdm():
     #     trainer.train(resume_from_checkpoint=config.resume_from_checkpoint)
@@ -71,8 +72,8 @@ def finetune(config: DictConfig) -> None:
 
     # model.save_pretrained(save_directory=config.model_dir)
 
-    # if hasattr(config.model, "use_decoder") and config.model.use_decoder:
-    #     train_and_store_ngram_model(config=config)
+    if hasattr(config.model, "use_decoder") and config.model.use_decoder:
+        train_and_store_ngram_model(config=config)
 
     if config.push_to_hub:
         push_model_to_hub(
