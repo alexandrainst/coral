d4e955939992d6269f72c454984e56f63ca2633f
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2024-10-23 17:06:29 +0200

    docs: Add TODO comment regarding the commenting of max length change in multigpu

--- src/scripts/finetune_asr_model.py
+++ src/scripts/finetune_asr_model.py
@@ -52,6 +52,9 @@ def main(config: DictConfig) -> None:
                     "training"
                 )
             config.model.layerdrop = 0.0
+
+        # TODO: This doesn't seem to be changed anymore, but keeping it here for some
+        #       time in case we need to re-enable it.
         # if config.padding != "max_length":
         #    if is_main_process:
         #        logger.info(
