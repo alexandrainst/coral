b6bfbdf2632530a5d5ab30c2561409ff255d3947
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-11-27 13:39:23 +0100

    chore: Remove unused function

--- src/coral/compute_metrics.py
+++ src/coral/compute_metrics.py
@@ -2,24 +2,15 @@
 
 import logging
 import os
-from collections.abc import Iterable
 
 import numpy as np
-from datasets import Dataset
 from numpy.typing import NDArray
-from tqdm.auto import tqdm
 from transformers import Wav2Vec2ProcessorWithLM
-from transformers.pipelines.automatic_speech_recognition import (
-    AutomaticSpeechRecognitionPipeline,
-)
-from transformers.pipelines.pt_utils import KeyDataset
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 from transformers.trainer_utils import EvalPrediction
 
-from .data import DEFAULT_CONVERSION_DICT, process_example
 from .data_models import Processor
 from .metrics import cer, wer
-from .utils import transformers_output_ignored
 
 logger = logging.getLogger(__package__)
 
@@ -98,70 +89,3 @@ def compute_error_rate_metrics(
         wer=wer(predictions=predictions_str, labels=labels_str),
     )
     return metrics
-
-
-def compute_metrics_of_dataset_using_pipeline(
-    dataset: Dataset,
-    transcriber: AutomaticSpeechRecognitionPipeline,
-    characters_to_keep: Iterable[str],
-    text_column: str,
-    audio_column: str,
-    batch_size: int,
-) -> tuple[list[str], list[str], dict[str, float]]:
-    """Compute the metrics for the dataset using a pipeline.
-
-    Args:
-        dataset:
-            The dataset to validate.
-        transcriber:
-            The transcriber used for transcribing the audio.
-        characters_to_keep:
-            The characters to keep in the transcriptions.
-        text_column:
-            The name of the column containing the transcriptions.
-        audio_column:
-            The name of the column containing the audio samples.
-        batch_size:
-            The batch size to use for transcribing the audio.
-
-    Returns:
-        A triple (predictions, labels, scores) where:
-            predictions:
-                The transcriptions predicted by the model.
-            labels:
-                The ASR-processed ground-truth labels for each sample.
-            scores:
-                A dictionary containing the computed scores for each metric.
-    """
-    # TEMP
-    dataset = dataset.shuffle(seed=42).select(range(100))
-
-    labels: list[str] = [lbl.strip().lower() for lbl in dataset[text_column]]
-    predictions: list[str] = list()
-    with (
-        tqdm(total=len(dataset), desc="Transcribing") as pbar,
-        transformers_output_ignored(),
-    ):
-        for out in transcriber(
-            KeyDataset(dataset=dataset, key=audio_column),  # type: ignore[arg-type]
-            batch_size=batch_size,
-            generate_kwargs=dict(language="danish", task="transcribe"),
-        ):
-            prediction = process_example(
-                example=dict(text=out["text"]),
-                characters_to_keep="".join(characters_to_keep),
-                conversion_dict=DEFAULT_CONVERSION_DICT,
-                text_column="text",
-                audio_column=None,
-                lower_case=True,
-                convert_numerals=True,
-                processor=None,
-            )["text"]
-            predictions.append(prediction)
-            pbar.update()
-
-    scores = dict(
-        cer=cer(predictions=predictions, labels=labels),
-        wer=wer(predictions=predictions, labels=labels),
-    )
-    return predictions, labels, scores
