a5502ab262b9726124ba0652488fcf26ff5adf76
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-12-04 14:58:09 +0100

    fix: Reinstate finetuning

--- src/coral/finetune.py
+++ src/coral/finetune.py
@@ -8,9 +8,10 @@ from transformers.trainer_callback import EarlyStoppingCallback
 
 from .data import load_data_for_finetuning
 from .data_models import ModelSetup
+from .experiment_tracking import ExTrackingSetup, load_extracking_setup
 from .model_setup import load_model_setup
 from .ngram import train_and_store_ngram_model
-from .utils import block_terminal_output, push_model_to_hub
+from .utils import block_terminal_output, disable_tqdm, push_model_to_hub
 
 logger = logging.getLogger(__package__)
 
@@ -31,10 +32,10 @@ def finetune(config: DictConfig) -> None:
     model = model_setup.load_model()
     dataset = load_data_for_finetuning(config=config, processor=processor)
 
-    # extracking_setup: ExTrackingSetup | None = None
-    # if config.enable_experiment_tracking and is_main_process:
-    #     extracking_setup = load_extracking_setup(config=config)
-    #     extracking_setup.run_initialization()
+    extracking_setup: ExTrackingSetup | None = None
+    if config.enable_experiment_tracking and is_main_process:
+        extracking_setup = load_extracking_setup(config=config)
+        extracking_setup.run_initialization()
 
     vals = {
         split_name: split
@@ -71,13 +72,13 @@ def finetune(config: DictConfig) -> None:
     )
 
     block_terminal_output()
-    # with disable_tqdm():
-    #     trainer.train(resume_from_checkpoint=config.resume_from_checkpoint)
+    with disable_tqdm():
+        trainer.train(resume_from_checkpoint=config.resume_from_checkpoint)
 
-    # if extracking_setup is not None and is_main_process:
-    #     extracking_setup.run_finalization()
+    if extracking_setup is not None and is_main_process:
+        extracking_setup.run_finalization()
 
-    # model.save_pretrained(save_directory=config.model_dir)
+    model.save_pretrained(save_directory=config.model_dir)
 
     if hasattr(config.model, "use_decoder") and config.model.use_decoder:
         train_and_store_ngram_model(config=config)
