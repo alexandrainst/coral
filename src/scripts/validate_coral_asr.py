5a7ae227ad68146f845efb0b2917948a37f7d3e2
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-12-05 14:10:42 +0100

    fix: Add missing argument

--- src/scripts/validate_coral_asr.py
+++ src/scripts/validate_coral_asr.py
@@ -52,6 +52,7 @@ def main(config: DictConfig) -> None:
 
     dataset = filter_dataset(
         dataset=dataset,
+        text_column=config.text_column,
         audio_column=config.audio_column,
         min_seconds_per_example=config.min_seconds_per_example,
         max_seconds_per_example=config.max_seconds_per_example,
