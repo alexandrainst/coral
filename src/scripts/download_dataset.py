4401fe225edf121399dca1bd71e9c1a8d7957ad6
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-12-19 14:59:26 +0000

    feat: Add subset option to download_dataset

--- src/scripts/download_dataset.py
+++ src/scripts/download_dataset.py
@@ -1,4 +1,11 @@
-"""Download a HF dataset to disk."""
+"""Download a HF dataset to disk.
+
+Usage:
+    uv run download_dataset.py \
+        --dataset-id DATASET_ID \
+        [--subset SUBSET] \
+        --output-dir OUTPUT_DIR
+"""
 
 from pathlib import Path
 
@@ -7,18 +14,36 @@ from datasets import load_dataset
 
 
 @click.command()
-@click.argument("dataset_id")
-@click.argument("output_dir")
-def download_dataset(dataset_id: str, output_dir: str) -> None:
+@click.option(
+    "--dataset-id",
+    type=str,
+    required=True,
+    help="The identifier of the dataset to download from the Hugging Face Hub.",
+)
+@click.option(
+    "--subset",
+    type=str,
+    default=None,
+    help="The subset of the dataset to download (if applicable).",
+)
+@click.option(
+    "--output-dir",
+    type=click.Path(file_okay=False, dir_okay=True, path_type=Path),
+    required=True,
+    help="The directory where the dataset directory will be saved.",
+)
+def download_dataset(dataset_id: str, subset: str | None, output_dir: Path) -> None:
     """Download a dataset from the Hugging Face Hub to disk.
 
     Args:
         dataset_id:
             The identifier of the dataset to download.
+        subset:
+            The subset of the dataset to download (if applicable).
         output_dir:
             The directory where the dataset will be saved.
     """
-    ds = load_dataset(dataset_id)
+    ds = load_dataset(path=dataset_id, name=subset)
     ds.save_to_disk(Path(output_dir, dataset_id.replace("/", "--")))
 
 
